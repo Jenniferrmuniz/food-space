d5100f9e48a6a475e4184791d3db69c155076933
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-10-24 09:00:17 -0400

    add new form fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 //may not use all of these
 const session = require('express-session');
@@ -19,16 +19,15 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 //const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-
 mongoose.Promise = Promise;
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+	.then((x) => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -43,24 +42,26 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+	require('node-sass-middleware')({
+		src: path.join(__dirname, 'public'),
+		dest: path.join(__dirname, 'public'),
+		sourceMap: true
+	})
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'food-space-icon.png')));
 
-
-app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: 'our-passport-local-strategy-app',
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
 app.use(passport.initialize());
 // this line is basically turning passport on
@@ -68,70 +69,61 @@ app.use(passport.initialize());
 app.use(passport.session());
 // this line connects the passport instance you just created, with the session that you just created above it
 
-
 app.use(flash());
 
-
 passport.serializeUser((user, cb) => {
-  cb(null, user._id);
+	cb(null, user._id);
 });
 
 passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
+	User.findById(id, (err, user) => {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, user);
+	});
 });
 
+passport.use(
+	new LocalStrategy((username, password, next) => {
+		User.findOne(
+			{
+				username
+			},
+			(err, user) => {
+				if (err) {
+					return next(err);
+				}
+				if (!user) {
+					return next(null, false, {
+						message: 'Incorrect username'
+					});
+				}
+				if (!bcrypt.compareSync(password, user.password)) {
+					return next(null, false, {
+						message: 'Incorrect password'
+					});
+				}
 
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, {
-        message: "Incorrect username"
-      });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, {
-        message: "Incorrect password"
-      });
-    }
-
-    return next(null, user);
-  });
-}));
-
-
+				return next(null, user);
+			}
+		);
+	})
+);
 
 //creates universal variable in all hbs files
-//creates user in session 
+//creates user in session
 app.use((req, res, next) => {
+	res.locals.theUser = req.user;
 
-  res.locals.theUser = req.user;
+	res.locals.successMessage = req.flash('success');
+	res.locals.errorMessage = req.flash('error');
 
-  res.locals.successMessage = req.flash('success');
-  res.locals.errorMessage = req.flash('error');
-
-  next();
-
-})
-
-
-
+	next();
+});
 
 // default value for title local
 app.locals.title = 'Welcome to FoodSpace';
-
-
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
@@ -145,13 +137,4 @@ app.use('/recipes', recipesRoutes);
 const commentRoutes = require('./routes/comment-routes');
 app.use('/comments', commentRoutes);
 
-
-
 module.exports = app;
-
-
-
-
-
-
-
